01aec8c98fbc0b936df5d1577e02210103cdccec
skonelm <skonelme@arcada.fi>
2022-11-07 14:42:19 +0200

    Initial commit!

---
+++ JS.js
@@ -0,0 +1,12 @@
+console.log("Hej från script in js!")
+
+function myFunctionality()
+{
+    console.log("The function worked");
+}
+
+myFunctionality();
+
+function buttonHandler(){
+    console.log("Du klickade här");
+}
